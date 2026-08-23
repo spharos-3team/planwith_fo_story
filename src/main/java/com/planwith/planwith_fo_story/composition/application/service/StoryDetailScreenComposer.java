00e2787356ebcc72b1b82cd1bf73a70628acf6bd
gnladu07 <gnlaud07@gmail.com>
2026-08-23 23:00:56 +0900

    feat: 컴파일 경고 해결

--- src/main/java/com/planwith/planwith_fo_story/composition/application/service/StoryDetailScreenComposer.java
+++ src/main/java/com/planwith/planwith_fo_story/composition/application/service/StoryDetailScreenComposer.java
@@ -2,7 +2,6 @@ package com.planwith.planwith_fo_story.composition.application.service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
